57afb4a5705718cf6952751f5764795cfcc93311
jaydenwen123 <2282186474@qq.com>
2020-06-19 14:39:46 +0800

    feat:add gomock library demo

--- go_mock/user_test.go
+++ go_mock/user_test.go
@@ -1,6 +1,10 @@
 package user
 
 import (
+	"errors"
+	"github.com/golang/mock/gomock"
+	//"golang.org/x/tools/go"
+	"log"
 	"reflect"
 	"testing"
 )
@@ -19,4 +23,47 @@ func TestNewUser(t *testing.T) {
 			}
 		})
 	}
+}
+
+// 静态设置返回值
+func TestReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repo := NewMockUserRepository(ctrl)
+	// 期望FindOne(1)返回张三用户
+	repo.EXPECT().FindOne(1).Return(&User{Name: "张三"}, nil)
+	// 期望FindOne(2)返回李四用户
+	repo.EXPECT().FindOne(2).Return(&User{Name: "李四"}, nil)
+	// 期望给FindOne(3)返回找不到用户的错误
+	repo.EXPECT().FindOne(3).Return(nil, errors.New("user not found"))
+	// 验证一下结果
+	log.Println(repo.FindOne(1)) // 这是张三
+	log.Println(repo.FindOne(2)) // 这是李四
+	log.Println(repo.FindOne(3)) // user not found
+	log.Println(repo.FindOne(4)) //没有设置4的返回值，却执行了调用，测试不通过
+}
+// 动态设置返回值
+func TestReturnDynamic(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	repo := NewMockUserRepository(ctrl)
+	// 常用方法之一：DoAndReturn()，动态设置返回值
+	repo.EXPECT().FindOne(gomock.Any()).DoAndReturn(func(i int) (*User,error) {
+		if i == 0 {
+			return nil, errors.New("user not found")
+		}
+		if i < 100 {
+			return &User{
+				Name:"小于100",
+			}, nil
+		} else {
+			return &User{
+				Name:"大于等于100",
+			}, nil
+		}
+	})
+	log.Println(repo.FindOne(120))
+	log.Println(repo.FindOne(66))
+	log.Println(repo.FindOne(0))
 }
